01d0738de70a02700b0897326f3fcc9154eb10b9
Kiet Nguyen Tuan <kietnt@nal.vn>
2026-06-10 14:16:36 +0700

    [task-04] posts: add admin & urls, update views

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Post, Tag
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug", "created_at"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+    list_per_page = 30
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "status", "created_at", "updated_at"]
+    list_filter = ["status", "created_at", "author"]
+    search_fields = ["title", "content", "author__username"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    filter_horizontal = ["tags"]
+    list_per_page = 20
+    ordering = ["-created_at"]
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related("author")
+            .prefetch_related("tags")
+        )
