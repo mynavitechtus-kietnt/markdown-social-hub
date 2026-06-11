e43da25cd8120b227b3244f23ac17b85a1cbcb56
Kiet Nguyen Tuan <kietnt@nal.vn>
2026-06-11 10:31:02 +0700

    [task-08] media_hub: create models & serializers

--- media_hub/apps.py
+++ media_hub/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class MediaHubConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'media_hub'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "media_hub"
+    verbose_name = "Media"
