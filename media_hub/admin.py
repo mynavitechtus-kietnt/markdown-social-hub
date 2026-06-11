937aec1e591c4f2806f77e707972b83aecb28945
Kiet Nguyen Tuan <kietnt@nal.vn>
2026-06-11 10:31:32 +0700

    [task-08] media_hub: create views & urls, regist control to admin

--- media_hub/admin.py
+++ media_hub/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Media
 
-# Register your models here.
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ["original_name", "uploaded_by", "file_size", "created_at"]
+    list_filter = ["created_at", "uploaded_by"]
+    search_fields = ["original_name", "uploaded_by__username"]
+    readonly_fields = ["id", "original_name", "file_size", "created_at"]
+    list_per_page = 20
