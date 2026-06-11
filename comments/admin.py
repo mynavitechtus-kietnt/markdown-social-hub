d6cd5f58a2840383737c4b7c2cad4dfb34b964a2
Kiet Nguyen Tuan <kietnt@nal.vn>
2026-06-11 10:26:39 +0700

    [task-07] comments: views & urls, regist control to admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["author", "post", "content_preview", "created_at"]
+    list_filter = ["created_at", "author"]
+    search_fields = ["content", "author__username", "post__title"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    list_per_page = 20
+
+    def content_preview(self, obj):
+        return obj.content[:60] + "..." if len(obj.content) > 60 else obj.content
+
+    content_preview.short_description = "Nội dung"
